e4be19bcb09432eb748ef7d6d8cfb978fc958934
rodrigopasa <rprodrigopasa@gmail.com>
2025-02-28 12:00:19 -0300

    Update requests.ts

--- src/utils/requests.ts
+++ src/utils/requests.ts
@@ -209,10 +209,8 @@ export const getLatestManga = async (): Promise<any[]> => {
               ?.attributes?.fileName
           }.256.jpg`
         ),
-        title:
-          k?.attributes?.title?.en ||
-          k?.attributes?.title?.["ja-ro"] ||
-          k?.attributes?.title?.ja,
+        title: k?.attributes?.title?.["pt-br"] || k?.attributes?.title?.["ja-ro"] || k?.attributes?.title?.ja,
+
       })) || []
     );
 
